3f3a7006a7071c34f5d5f34ad609752c4fe61455
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-10-27 02:31:04 +0300

    fv

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:*', 
+  origin: 'http://localhost:5173', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true 
 }));
