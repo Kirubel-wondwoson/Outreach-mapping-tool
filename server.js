458bafbc7b0ec0ebfd958d3d5ffb1a2751660f27
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-10-27 02:47:49 +0300

    fv

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const connectDB = require('./config/db')
 require("dotenv").config()
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: 'http://127.0.0.1:*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true 
+}));
 
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.json({extended: false}))
+app.use(cors())
 
 connectDB()
 
